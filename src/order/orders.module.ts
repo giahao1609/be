681e7a8f492d5ba9acb92cd99da6090f154c96dc
tanQuang1 <huynhtanquang.htq1@gmail.com>
2025-12-12 08:13:19 +0700

    fix

--- src/order/orders.module.ts
+++ src/order/orders.module.ts
@@ -13,13 +13,14 @@ import { User, UserSchema } from 'src/users/schema/user.schema';
 import { UsersService } from 'src/users/users.service';
 import { UploadService } from 'src/upload/upload.service';
 import { MailerModule } from '@nestjs-modules/mailer';
-import { ConfigService } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { join } from 'path';
 import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
 
 
 @Module({
   imports: [
+    ConfigModule,
     HttpModule,
     MongooseModule.forFeature([
       { name: PreOrder.name, schema: PreOrderSchema },
